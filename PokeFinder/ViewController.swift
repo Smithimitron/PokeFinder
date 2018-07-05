b1e729d17fa1a210571739b4c9b8a408fa28ed97
Gavin macOS HS <gavincraft2016@gmail.com>
2018-07-05 14:58:06 -0400

    Initial Commit

---
+++ PokeFinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PokeFinder
+//
+//  Created by Gavin Craft on 7/5/18.
+//  Copyright © 2018 Gavin Craft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
